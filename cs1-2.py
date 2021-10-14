2335068ba7ef8d2358b66dd7eb25b09b7872cc8d
p22mgiroux <p22mgiroux@fairfieldprep.org>
2021-10-14 09:50:06 -0400

    Fix order of operations by moving one parentheses

--- cs1-2.py
+++ cs1-2.py
@@ -4,6 +4,6 @@ invest_amnt = input("What is your investment amount? ")
 interest_rate = input("What is the annual interest rate (as a decimal)? ")
 time = input("How many years is the money in the account? ")
 
-future_value = float(invest_amnt) * ((1 + float(interest_rate) / 12) ** 12 * float(time))
+future_value = float(invest_amnt) * (1 + float(interest_rate) / 12) ** (12 * float(time))
 
 print("The future favue of your investment after " + time + " years, is $" + str(future_value) + ".")
